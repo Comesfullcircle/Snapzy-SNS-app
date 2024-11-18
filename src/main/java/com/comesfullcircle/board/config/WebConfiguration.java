32612d63c493da1d07e176d1a1cdd6caee18ea51
윤설이 <olerlmin1234@gmail.com>
2024-11-18 19:38:12 +0900

    CORS 관련 설정

--- src/main/java/com/comesfullcircle/board/config/WebConfiguration.java
+++ src/main/java/com/comesfullcircle/board/config/WebConfiguration.java
@@ -8,16 +8,40 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 
 @Configuration
 public class WebConfiguration {
+
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(
+                List.of("http://localhost:3000", "http://127.0.0.1:3000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.addExposedHeader("Authorization"); // Authorization 헤더 허용
+        configuration.setAllowCredentials(true); // 인증 정보(Cookies) 허용
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 적용
+        return source;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(Customizer.withDefaults()) // CORS 설정 활성화
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/api/v1/public/**").permitAll() // 인증 없이 허용
+                        .anyRequest().authenticated()) // 나머지는 인증 필요
                 .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화 수정
-                .httpBasic(Customizer.withDefaults());
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
+                .httpBasic(Customizer.withDefaults()); // HTTP Basic 인증
 
         return http.build();
     }
