593af70083336387c72ba5b46ba5642c75580a76
윤설이 <olerlmin1234@gmail.com>
2024-11-22 18:09:31 +0900

    유저 API 개발

--- src/main/java/com/comesfullcircle/board/controller/UserController.java
+++ src/main/java/com/comesfullcircle/board/controller/UserController.java
@@ -1,25 +1,56 @@
 package com.comesfullcircle.board.controller;
 
-import com.comesfullcircle.board.model.user.User;
-import com.comesfullcircle.board.model.user.UserAuthenticationResponse;
-import com.comesfullcircle.board.model.user.UserLoginRequestBody;
-import com.comesfullcircle.board.model.user.UserSignUpRequestBody;
+import com.comesfullcircle.board.model.entity.UserEntity;
+import com.comesfullcircle.board.model.post.Post;
+import com.comesfullcircle.board.model.user.*;
+import com.comesfullcircle.board.service.PostService;
 import com.comesfullcircle.board.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
     @Autowired
     UserService userService;
+    @Autowired
+    private PostService postService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getUsers(@RequestParam(required = false) String query){
+        var users = userService.getUsers(query);
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getUser(@PathVariable String username){
+        var user = userService.getUser(username);
+        return ResponseEntity.ok(user);
+    }
+
+    @PatchMapping("/{username}")
+    public ResponseEntity<User> updateUser(
+            @PathVariable String username,
+            @RequestBody UserPatchRequestBody requestBody,
+            Authentication authentication){
+        var user =
+                userService.updateUser(username, requestBody, (UserEntity) authentication.getPrincipal());
+        return ResponseEntity.ok(user);
+    }
+
+    //GET /users/{username}/posts
+    @GetMapping("/{username}/posts")
+    public ResponseEntity<List<Post>> getPostByUsername(@PathVariable String username){
+        var posts = postService.getPostsByUsername(username);
+        return ResponseEntity.ok(posts);
+    }
 
     @PostMapping
     public ResponseEntity<User> signUp(@Valid @RequestBody UserSignUpRequestBody userSignUpRequestBody){
